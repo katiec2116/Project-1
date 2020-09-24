2514ea410dd6f21c5bef05feecbff65bd465e60b
katiec <katiecampbell21@outlook.com>
2020-09-23 20:24:38 -0400

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,11 @@
 
 let searchedFood;
 
+// on click function for submitting food
 $("#foodBtn").on("click", function (event) {
     event.preventDefault();
+    // var to hold sumbitted food
     searchedFood = $("#food").val();
-
     var queryURL = "https://trackapi.nutritionix.com/v2/natural/nutrients";
     // call to api to generate data
     $.ajax({
@@ -23,16 +24,9 @@ $("#foodBtn").on("click", function (event) {
 
         // do stuff with the data
         .then(function (response) {
-            console.log(response.foods[0])
+            // loops through all foods found and add calories count to the page
             for (var i = 0; i < response.foods.length; i++) {
                 $("#results").append($("<p>").text(response.foods[i].food_name + " Calories: " + response.foods[i].nf_calories))
             }
-
-
-
         });
-
-
-
-
 });
