8135124f2b440a950d3d8b79f582b4e96186826d
killermitch <anthonnymitch22@gmail.com>
2020-09-25 17:29:58 -0400

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,25 @@
 // grabbing button by ID and creating an event
 
 let searchedFood;
+let breakfast = [];
+let lunch = [];
+let dinner = [];
 
 
+// get foods from local storage
+getFoods();
+
+// clear button to empty local storage and display
+$("#clear").on("click", function (event) {
+    event.preventDefault();
+    $(".content2").empty();
+    $(".contentLunch").empty();
+    $(".contentDinner").empty();
+    localStorage.clear();
+});
 // on click function for submitting food (Breakfast)
 $("#foodBtnBreakfast").on("click", function (event) {
-    event.preventDefault();
+    event.preventDefault()
     // var to hold sumbitted food
     searchedFood = $("#food").val();
     var queryURL = "https://trackapi.nutritionix.com/v2/natural/nutrients";
@@ -25,9 +39,22 @@ $("#foodBtnBreakfast").on("click", function (event) {
 
         // do stuff with the data
         .then(function (response) {
+            console.log(response)
             // loops through all foods found and add calories count to the page
             for (var i = 0; i < response.foods.length; i++) {
-                $(".content2").append($("<p>").text(response.foods[i].food_name + " Calories: " + response.foods[i].nf_calories))
+                // grabs food name, and calories
+                let d = {
+                    name: response.foods[i].food_name,
+                    calories: response.foods[i].nf_calories
+                }
+                // capitalizes food
+                d.name = d.name.charAt(0).toUpperCase() + d.name.substr(1);
+                // add to page
+                $(".content2").append($("<p>").text(d.name + " Calories: " + d.calories));
+                // adds to array
+                breakfast.push(d)
+                // saves food to local storage
+                saveFoods();
             }
         });
 });
@@ -56,7 +83,19 @@ $("#foodBtnLunch").on("click", function (event) {
         .then(function (response) {
             // loops through all foods found and add calories count to the page
             for (var i = 0; i < response.foods.length; i++) {
-                $(".contentLunch").append($("<p>").text(response.foods[i].food_name + " Calories: " + response.foods[i].nf_calories))
+                // grabs food name, and calories
+                let d = {
+                    name: response.foods[i].food_name,
+                    calories: response.foods[i].nf_calories
+                }
+                // capitalizes food
+                d.name = d.name.charAt(0).toUpperCase() + d.name.substr(1);
+                // add to page
+                $(".contentLunch").append($("<p>").text(d.name + " Calories: " + d.calories));
+                // adds to array
+                lunch.push(d)
+                // saves food to local storage
+                saveFoods();
             }
         });
 });
@@ -66,6 +105,7 @@ $("#foodBtnDinner").on("click", function (event) {
     event.preventDefault();
     // var to hold sumbitted food
     searchedFood = $("#foodDinner").val();
+    // dinner.push(searchedFood);
     var queryURL = "https://trackapi.nutritionix.com/v2/natural/nutrients";
     // call to api to generate data
     $.ajax({
@@ -85,69 +125,168 @@ $("#foodBtnDinner").on("click", function (event) {
         .then(function (response) {
             // loops through all foods found and add calories count to the page
             for (var i = 0; i < response.foods.length; i++) {
-                $(".contentDinner").append($("<p>").text(response.foods[i].food_name + " Calories: " + response.foods[i].nf_calories))
+                // grabs food name and calories
+                let d = {
+                    name: response.foods[i].food_name,
+                    calories: response.foods[i].nf_calories
+                }
+                // capitalizes food
+                d.name = d.name.charAt(0).toUpperCase() + d.name.substr(1);
+                // add to page
+                $(".contentDinner").append($("<p>").text(d.name + " Calories: " + d.calories));
+                // adds to array
+                dinner.push(d);
+                // saves food to local storage
+                saveFoods();
             }
         });
 });
+
+// Adds Calories together 
+$("#totalBtn").on("click",
+
+    function getTotalCal() {
+        var total = 0;
+        for (let index = 0; index < breakfast.length; index++) {
+            total += parseFloat(breakfast[index].calories)
+        }
+        for (let index = 0; index < lunch.length; index++) {
+            total += parseFloat(lunch[index].calories)
+        }
+        for (let index = 0; index < dinner.length; index++) {
+            total += parseFloat(dinner[index].calories)
+        }
+
+
+        console.log(total);
+
+
+        $(".totalCalDisplay").append($("<p>").text(total));
+    });
+
+
+// local storage for food data
+function saveFoods() {
+    localStorage.setItem("breakfast", JSON.stringify(breakfast));
+    localStorage.setItem("lunch", JSON.stringify(lunch));
+    localStorage.setItem("dinner", JSON.stringify(dinner));
+}
+
+function getFoods() {
+    // checks if local storage is empty
+    if (localStorage.getItem("breakfast") != null) {
+        breakfast = JSON.parse(localStorage.getItem("breakfast"));
+        // generate p tag to display each food item
+        breakfast.forEach(item => {
+            let food = $("<p>").text(item.name + " Calories: " + item.calories);
+            // add city to search history lost and add data value attribute
+            $(".content2").append(food);
+        })
+    }
+    // checks if local storage is empty
+    if (localStorage.getItem("lunch") != null) {
+        lunch = JSON.parse(localStorage.getItem("lunch"));
+        // generate p tag to display each food item
+        lunch.forEach(item => {
+            let food = $("<p>").text(item.name + " Calories: " + item.calories);
+            // add city to search history lost and add data value attribute
+            $(".contentLunch").append(food);
+        })
+    }
+    // checks if local storage is empty
+    if (localStorage.getItem("dinner") != null) {
+        dinner = JSON.parse(localStorage.getItem("dinner"));
+        // generate p tag to display each food item
+        dinner.forEach(item => {
+            let food = $("<p>").text(item.name + " Calories: " + item.calories);
+            // add city to search history lost and add data value attribute
+            $(".contentDinner").append(food);
+        })
+    }
+}
+//..................................................................///////////////.............................///////////////////................////////.......
+
+let apiKey = "515c74fb86mshcb44e437cf75abcp1b8dc7jsn1ac8f5643c83";
+let buttonS = $(".summitBnt");
+
+
+
+let dailyCaloryEl = $(".dailyCalories");
+let bmiEl = $(".bmi");
+let idealWeightEl = $(".idealWeight");
+let bodyFatEl = $(".bodyFat");
+
+
+
+
+
 //. Daily Calories 
 var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://fitness-calculator.p.rapidapi.com/dailycalory?heigth=100&age=20&gender=male&weigth=170",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
-		"x-rapidapi-key": "515c74fb86mshcb44e437cf75abcp1b8dc7jsn1ac8f5643c83"
-	}
+    "async": true,
+    "crossDomain": true,
+    "url": "https://fitness-calculator.p.rapidapi.com/dailycalory?heigth=100&age=20&gender=male&weigth=170",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
+        "x-rapidapi-key": "515c74fb86mshcb44e437cf75abcp1b8dc7jsn1ac8f5643c83"
+    }
 }
 
 $.ajax(settings).done(function (response) {
-	console.log(response);
+    console.log(response);
+    $(".resultados").text(response.data.BMR)
 });
-//.Boy mass Index
+
+
+//.Body mass Index
 var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://fitness-calculator.p.rapidapi.com/bmi?age=25&height=180&weight=65",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
-		"x-rapidapi-key": "515c74fb86mshcb44e437cf75abcp1b8dc7jsn1ac8f5643c83"
-	}
+    "async": true,
+    "crossDomain": true,
+    "url": "https://fitness-calculator.p.rapidapi.com/bmi?age=25&height=180&weight=65",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
+        "x-rapidapi-key": "515c74fb86mshcb44e437cf75abcp1b8dc7jsn1ac8f5643c83"
+    }
 }
 
 $.ajax(settings).done(function (response) {
-	console.log(response);
+    console.log(response);
+    $(".resultados").text(response.bmi);
 });
 
 //. Ideal weight 
 var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://fitness-calculator.p.rapidapi.com/idealweight?weight=70&gender=male&height=178",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
-		"x-rapidapi-key": "515c74fb86mshcb44e437cf75abcp1b8dc7jsn1ac8f5643c83"
-	}
+    "async": true,
+    "crossDomain": true,
+    "url": "https://fitness-calculator.p.rapidapi.com/idealweight?weight=70&gender=male&height=178",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
+        "x-rapidapi-key": "515c74fb86mshcb44e437cf75abcp1b8dc7jsn1ac8f5643c83"
+    }
 }
 
 $.ajax(settings).done(function (response) {
-	console.log(response);
+    console.log(response);
+    $(".resultados").text(response.Devine)
+
 });
 //. Body Fat.
 var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://fitness-calculator.p.rapidapi.com/bodyfat?waist=96&gender=male&neck=50&heigth=178&hip=92&age=25&weigth=70",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
-		"x-rapidapi-key": "515c74fb86mshcb44e437cf75abcp1b8dc7jsn1ac8f5643c83"
-	}
+    "async": true,
+    "crossDomain": true,
+    "url": "https://fitness-calculator.p.rapidapi.com/bodyfat?waist=96&gender=male&neck=50&heigth=178&hip=92&age=25&weigth=70",
+    "method": "GET",
+    "headers": {
+        "x-rapidapi-host": "fitness-calculator.p.rapidapi.com",
+        "x-rapidapi-key": "515c74fb86mshcb44e437cf75abcp1b8dc7jsn1ac8f5643c83"
+    }
 }
 
 $.ajax(settings).done(function (response) {
-	console.log(response);
+    console.log(response);
+    $(".resultados").text(response)
 });
+
 
